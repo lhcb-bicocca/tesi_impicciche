0cf67515b05e87a5cec42acd84f45d8256db1e90
Maurizio Martinelli <martinelli.maurizio@gmail.com>
2026-07-29 12:47:57 +0200

    added example library and macro

---
+++ macros/run_myfunc.py
@@ -0,0 +1,8 @@
+from mypkg.library import myfunc
+
+def main():
+    myfunc()
+    return
+
+if __name__ == "__main__":
+    main()
---
+++ src/mypkg/library.py
@@ -0,0 +1,6 @@
+def myfunc():
+    """
+    This is a sample function that performs a simple task.
+    """
+    print("Hello from myfunc!")
+    return
